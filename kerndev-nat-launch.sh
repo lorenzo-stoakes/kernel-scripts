20ee1d74257333cdf7ba050575e2315839409888
Lorenzo Stoakes <lstoakes@gmail.com>
2015-05-23 22:35:02 +0100

    Make 3rd party scripts use consistent spacing.

--- kerndev-nat-launch.sh
+++ kerndev-nat-launch.sh
@@ -5,23 +5,22 @@ set -e; set -o pipefail
 # http://xyne.archlinux.ca/notes/network/dhcp_with_dns.html
 
 function print_usage() {
-    echo "usage: $0 <WAN interface> <subnet interface> <up|down>"
+	echo "usage: $0 <WAN interface> <subnet interface> <up|down>"
 }
 
 if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root." >&2
-    exit 1
+	echo "This script must be run as root." >&2
+	exit 1
 fi
 
 if [[ -z $3 ]]; then
-    print_usage
-    exit 1
+	print_usage
+	exit 1
 else
-    wan_nic="$1"
-    subnet_nic="$2"
-    action="$3"
+	wan_nic="$1"
+	subnet_nic="$2"
+	action="$3"
 fi
-
 
 mask=/24
 subnet_ip=192.168.1.0$mask
