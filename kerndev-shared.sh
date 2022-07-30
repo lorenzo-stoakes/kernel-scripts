348ed296088c290c58a4e2d844419f033e181eb7
Lorenzo Stoakes <lstoakes@gmail.com>
2022-07-30 14:24:32 +0100

    set defaults after functions

--- kerndev-shared.sh
+++ kerndev-shared.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e; set -o pipefail
 
-source kerndev-defaults.sh
 source kerndev-functions.sh
+source kerndev-defaults.sh
 
 # Fixup the LINUX_DEV_PATH.
 LINUX_DEV_PATH="$(find_base_linux_path $LINUX_DEV_PATH)"
